7739db5f16b9dc3680b9125e69ed26cb8d7dda2e
Mehtab Riar <mehtabriar228@hotmail.com>
2022-08-04 00:22:46 -0700

    a bit of java

---
+++ script.js
@@ -0,0 +1,4 @@
+let userName = prompt("Hey, what's your name?");
+document.write("Hey " + userName + ",Welcome to Seattle!")
+prompt(userName  + " do you like to hike? ")
+if (yes) alert("You've Come to the Right Place!")
