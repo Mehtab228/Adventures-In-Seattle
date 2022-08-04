58743e6be5aa39f33c1e261f5da5b98183bf27a2
Mehtab Riar <mehtabriar228@hotmail.com>
2022-08-04 12:52:20 -0700

    added some prompt boxes and responses

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-let userName = prompt("Hey, what's your name?");
-document.write("Hey " + userName + ",Welcome to Seattle!");
-let response = prompt(userName  + " do you like to hike? ");
-if (response = yes) alert{"You've Come to the Right Place!"};
+//let userName = prompt("Hey, what's your name?");
+//document.write("Hey " + userName + ",Welcome to Seattle!");
+//let response = prompt(userName  + " do you like to hike? ");
+//if (response = yes) alert{"You've Come to the Right Place!"};
+
+function getName(){
+    let userName = prompt("What is your name?");
+    if (userName == "" || userName == null || userName == undefined) {
+        alert("Nice try Kassie")
+    }
+    document.write("Hey " + userName + ",Welcome to Seattle!");
+}
+
+getName()
+
+function ask1(){
+    let response = prompt("Do you like to hike?")
+    if (response == "" || response == null || response == undefined){
+        alert("Nice try Kassie")
+    }
+    else if (response == "yes"){
+        alert("You've come to the right place!")
+    }
+    else if (response == "no"){
+        prompt("Do you like to kayak")
+    }
+    else {
+        alert ("Next time, please type yes or no")
+    }
+}
+
+ask1()
+
