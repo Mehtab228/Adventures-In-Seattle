db2f1e51cbf424657403863a9733bf237c9a1604
Mehtab Riar <mehtabriar228@hotmail.com>
2022-08-04 12:03:06 -0700

    Added chris's link

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let userName = prompt("Hey, what's your name?");
-document.write("Hey " + userName + ",Welcome to Seattle!")
-prompt(userName  + " do you like to hike? ")
-if (yes) alert("You've Come to the Right Place!")
+document.write("Hey " + userName + ",Welcome to Seattle!");
+let response = prompt(userName  + " do you like to hike? ");
+if (response = yes) alert{"You've Come to the Right Place!"};
